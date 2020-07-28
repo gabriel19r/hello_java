7f29a5d8aa70d59553f24ea469c297ddcab9e08d
Gabriel Rodrigues <grodriqui@gmail.com>
2020-07-27 23:42:04 -0300

    Adicionado a função de substituição de endereço ou telefone

--- src/arquivos_sequenciais/Arquivo_sequencial.java
+++ src/arquivos_sequenciais/Arquivo_sequencial.java
@@ -4,9 +4,9 @@ import javax.swing.JOptionPane;
 import java.io.*;
 
 public class Arquivo_sequencial {
-    static StringBuffer memoria; //do exemplo 2
-    public static void main(String[] args)
-    {
+    static StringBuffer memoria; //do exemplo 2 e 3
+    static String arq=Selecionando_Arquivo.caminho(); //do exemplo 3
+    public static void main(String[] args) {
         // - Exemplo 1
         /*
 
@@ -38,7 +38,8 @@ public class Arquivo_sequencial {
          */
 
         // - Exemplo 2
-        /**/
+        /*
+
         try{
             String arq=Selecionando_Arquivo.caminho();
             BufferedReader arqEntrada=new BufferedReader(new FileReader(arq));
@@ -88,5 +89,86 @@ public class Arquivo_sequencial {
         dados=memoria.substring(p,u);
 
         return dados;
+    }
+
+    //*/
+
+        // - Exemplo 3
+
+        try{
+        BufferedReader arqEntrada=new BufferedReader(new FileReader(arq));
+
+        String nome=JOptionPane.showInputDialog("Digite o nome");
+        nome=nome.toUpperCase();
+
+        String linha="";
+        String endereco="";
+        String telefone="";
+
+        while((linha=arqEntrada.readLine())!=null){
+            memoria.append(linha+"\r\n");
+        }
+
+        int inicio=-1;
+        inicio=memoria.indexOf(nome);
+
+        if(inicio!=-1){
+            int ultimo=memoria.indexOf("\t",inicio);
+            nome=ler(inicio,ultimo);
+
+            int primeiro=ultimo+1;
+            ultimo=memoria.indexOf("\t",primeiro);
+
+            endereco=ler(primeiro,ultimo);
+
+            primeiro=ultimo+1;
+
+            int fim=memoria.indexOf("\n",primeiro);
+            telefone=ler(primeiro,fim);
+
+            Agenda agenda1=new Agenda(nome,endereco,telefone);
+
+            JOptionPane.showMessageDialog(null,agenda1.getEndereco()+"\n"+agenda1.getTelefone());
+
+            endereco=JOptionPane.showInputDialog("Entre com o novo endereço");
+            endereco=endereco.toUpperCase();
+            agenda1.setEndereco(endereco);
+
+            telefone=JOptionPane.showInputDialog("Entre com o novo telefone");
+            agenda1.setTelefone(telefone);
+
+            memoria.replace(inicio,fim+1,
+                    agenda1.getNome()+"\t"+
+                    agenda1.getEndereco()+"\t"+
+                    agenda1.getTelefone()+"\r\n");
+            gravar();
+            JOptionPane.showMessageDialog(null,"Atualização realizada com sucesso");
+        }else{
+            JOptionPane.showMessageDialog(null,"Não cadastrado");
+        }
+        arqEntrada.close();
+
+    }catch(Exception e){
+        JOptionPane.showMessageDialog(null,"Erro de leitura");
+    }
+}
+
+public static String ler(int p,int u){
+    String dados="";
+    dados=memoria.substring(p,u);
+    return dados;
+}
+
+public static void gravar(){
+    try{
+        BufferedWriter saida=new BufferedWriter(new FileWriter(arq));
+
+        saida.write(memoria.toString());
+
+        saida.flush();
+        saida.close();
+    }catch(Exception e){
+        JOptionPane.showMessageDialog(null, "Erro de gravação");
+        }
     }
 }
